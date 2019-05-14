e4cd7935ac907cb23af250b3ec6f5d12a37a2034
jimmyprod <48336150+jimmyprod@users.noreply.github.com>
2019-05-14 23:16:31 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require("./config.json");
 
 client.on("ready", () => {
   console.log("Je suis prêt !");
 });
 
-client.login(config.token);
+client.login(process.env.TOKEN);
