9da3f2192f7f271d952873691000b70677daceee
jimmyprod <48336150+jimmyprod@users.noreply.github.com>
2019-05-16 20:21:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on("ready", () => {
-  console.log("Je suis prêt !");
-});
+let prefix = "!"
 
 client.login(process.env.TOKEN);
-var prefix = ("!");
+
+//Quand le bot ce lance
+client.on("ready", () => {
+  console.log("Je suis prêt !")
+  client.user.setActivity("Vous aidez")
+})
+//Quand un membre rejoin
 client.on('guildMemberAdd', function (member) {
   let embed = new Discord.RichEmbed()
       .setDescription(':tada: **' + member.user.username + '** a rejoint ' + member.guild.name)
       .setFooter('Nous sommes désormais ' + member.guild.memberCount)
-  member.guild.channels.get('577602282120806407').send(embed)
+  member.guild.channels.get('577604604590817280').send(embed)
   member.addRole('577972770776809482')
+
 })
 
+// quand un membre quitte
 client.on('guildMemberRemove', function (member) {
   let embed = new Discord.RichEmbed()
       .setDescription(':cry: **' + member.user.username + '** a quitté ' + member.guild.name)
       .setFooter('Nous sommes désormais ' + member.guild.memberCount)
   member.guild.channels.get('577974964393607198').send(embed)
+
 })
-  if (MessageChannel.content.includes("!help")){
-var help_embed = new Discord.RichEmbed()
-.setTitle("HELP")
-.setColor("#02FB24")
-.setDescription(":robot: Voici les commandes du bot :")
-.addField("Citoyen(ne)", "!help")
-message.channel.send(help_embed)
-}
+
+//Commandes test bot
+client.on("message", message =>{
+  if(message.content === prefix + "test") {
+   message.channel.send("Le bot fonctionne correctement.")
+   
+  }
+})
