b046ab971192b4011ee25e26e32aa16f17c73223
jimmyprod <48336150+jimmyprod@users.noreply.github.com>
2019-05-17 20:12:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ client.on('guildMemberAdd', function (member) {
   let embed = new Discord.RichEmbed()
       .setDescription(':tada: **' + member.user.username + '** a rejoint ' + member.guild.name)
       .setFooter('Nous sommes désormais ' + member.guild.memberCount)
-  member.guild.channels.get('577604604590817280').send(embed)
+  member.guild.channels.get('577602282120806407').send(embed)
   member.addRole('577972770776809482')
 
 })
